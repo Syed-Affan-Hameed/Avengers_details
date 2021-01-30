ec5125d505ddecdb994922f560617dc5d15379c9
Syed_Affan_Hameed <syedaffanhameed@gmail.com>
2021-01-31 01:22:31 +0530

    Working commit-2

--- app/src/main/java/com/syed/activitylifecyle/LoginActivity.kt
+++ app/src/main/java/com/syed/activitylifecyle/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.syed.activitylifecyle
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +10,7 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
-class LoginActivity : AppCompatActivity() /*View.OnClickListener*/  {
+class LoginActivity : AppCompatActivity() /*View.OnClickListener*/ {
 /*override fun onClick(v: View?) {
         Toast.makeText(this@LoginActivity, "Login Button not operational Yet", Toast.LENGTH_LONG)
             .show()
@@ -20,15 +21,22 @@ class LoginActivity : AppCompatActivity() /*View.OnClickListener*/  {
     lateinit var btnlogin: Button
     lateinit var txtforgotpass: TextView
     lateinit var txtregister: TextView
-    val useridphone:String ="012345"
-    val password=arrayOf("tony","widow","hulk","thor","wakanda","steve","clint")
-
+    val useridphone: String = "012345"
+    val password = arrayOf("tony", "widow", "hulk", "thor", "wakanda", "steve", "clint")
+    lateinit var sharedpreferences: SharedPreferences
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_login)
+        sharedpreferences =
+            getSharedPreferences(getString(R.string.preferences_file_name), Context.MODE_PRIVATE)
+        val isLoggedIn = sharedpreferences.getBoolean("isLoggedIn", false)
+        if (isLoggedIn) {
+            intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            setContentView(R.layout.activity_login)
+        }
         title = "Login"
         etemail = findViewById(R.id.etemail)
         etpass = findViewById(R.id.etpass)
@@ -45,36 +53,43 @@ class LoginActivity : AppCompatActivity() /*View.OnClickListener*/  {
             if (user == useridphone) {
                 when (pass) {
                     password[0] -> {
+                        savepreferences()
                         avenger = "Iron Man"
                         intent.putExtra("AvengerName", avenger)
                         startActivity(intent)
                     }
                     password[1] -> {
+                        savepreferences()
                         avenger = "Black Widow"
                         intent.putExtra("AvengerName", avenger)
                         startActivity(intent)
                     }
                     password[2] -> {
+                        savepreferences()
                         avenger = "The Incredible Hulk (Strongest avenger;))"
                         intent.putExtra("AvengerName", avenger)
                         startActivity(intent)
                     }
                     password[3] -> {
+                        savepreferences()
                         avenger = "THOR(Point Break)"
                         intent.putExtra("AvengerName", avenger)
                         startActivity(intent)
                     }
                     password[4] -> {
+                        savepreferences()
                         avenger = "Black Panther"
                         intent.putExtra("AvengerName", avenger)
                         startActivity(intent)
                     }
                     password[5] -> {
+                        savepreferences()
                         avenger = "Captain America"
                         intent.putExtra("AvengerName", avenger)
                         startActivity(intent)
                     }
                     password[6] -> {
+                        savepreferences()
                         avenger = "Hawk Eye"
                         intent.putExtra("AvengerName", avenger)
                         startActivity(intent)
@@ -91,9 +106,14 @@ class LoginActivity : AppCompatActivity() /*View.OnClickListener*/  {
             }
         }
     }
+
     override fun onPause() {
         super.onPause()
         finish()
+    }
+
+    fun savepreferences() {
+        sharedpreferences.edit().putBoolean("isLoggedIn", true).apply()
     }
 
 }
